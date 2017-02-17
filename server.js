2e42ec3b37c7bd5f297d0662ecfe4596ebdf979a
smitakagwade <smitakagwade26@gmail.com>
2017-02-17 15:15:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ app.use(morgan('combined'));
 app.get('/article-one', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'articleOne.html'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
