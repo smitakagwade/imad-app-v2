30037b1a4a795eb10e620cbd9cf81f99dca87e7c
smitakagwade <smitakagwade26@gmail.com>
2017-03-19 14:09:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(res, req){
+/*app.get('/article-one',function(res, req){
 res.send('article one requested and will be served here')
 });
 app.get('/article-two',function(res, req){
@@ -16,7 +16,7 @@ res.send('article two requested and will be served here');
 });
 app.get('/article-three',function(res, req){
 res.send('article three requested and will be served here'); 
-});
+});*/
 app.get('/ui/style.css', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
