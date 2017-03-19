1147329184d516098703f92002280d331098ba94
smitakagwade <smitakagwade26@gmail.com>
2017-03-19 14:22:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(res, req){
-res.send('article one requested and will be served here');
+res.sendFile('article one requested and will be served here');
 });
 app.get('/article-two',function(res, req){
-res.send('article two requested and will be served here'); 
+res.sendFile('article two requested and will be served here'); 
 });
 app.get('/article-three',function(res, req){
-res.send('article three requested and will be served here'); 
+res.sendFile('article three requested and will be served here'); 
 });
 app.get('/ui/style.css', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'style.css'));
