7519ce13043954cb903802ed3a13250096823dc6
smitakagwade <smitakagwade26@gmail.com>
2017-03-22 16:17:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,6 +30,18 @@ app.get('/test-db',function(req,res){
     }
      });
 });
+app.get('/articles/:articleName',function(req,res){
+    pool.query("select * from article where title="+req.params.articleName,function(err,result){
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+           res.send(JSON.stringify(result));
+        }
+    });
+})
 
 /*app.get('/Article-one', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
